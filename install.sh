bef86097dc3176e7fb9f8b6a6ce3e8a6cbfd6322
vinsion <vinsion@mail.com>
2019-07-10 14:41:02 +0800

    init-1

---
+++ install.sh
@@ -0,0 +1,10 @@
+##########################################################################
+# File Name: install.sh
+# Author: vinsion
+# mail: vinsion@vinsion.mail
+# Created Time: Wed 10 Jul 2019 11:28:17 AM CST
+#########################################################################
+#!/bin/sh
+
+cp .vim .vimrc .vimrc.ok .viminfo ~/ -rf
+exit 0
